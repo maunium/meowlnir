b0ebe13dcc3d7caf59ea7bdb16770b09e3968bf0
Tulir Asokan <tulir@maunium.net>
2025-03-24 13:11:44 +0200

    policyeval/messagehandle: add primitive auto-redact for media

--- config/upgrade.go
+++ config/upgrade.go
@@ -35,6 +35,7 @@ func upgradeConfig(helper up.Helper) {
 	helper.Copy(up.Str|up.Null, "meowlnir", "report_room")
 	helper.Copy(up.List, "meowlnir", "hacky_rule_filter")
 	helper.Copy(up.Bool, "meowlnir", "filter_local_invites")
+	helper.Copy(up.Str|up.Null, "meowlnir", "media_repo_token")
 
 	if secret, ok := helper.Get(up.Str, "meowlnir", "antispam_secret"); ok && secret != "generate" {
 		helper.Set(up.Str, secret, "antispam", "secret")
