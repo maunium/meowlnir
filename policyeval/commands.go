20d765dedbecd178702bfefe179b48d60920532b
nexy7574 <git@nexy7574.co.uk>
2025-08-31 17:55:18 +0100

    Implement msc4323 suspension
    # Conflicts:
    #	go.mod
    #	go.sum

--- policyeval/commands.go
+++ policyeval/commands.go
@@ -896,9 +896,14 @@ var cmdSuspend = &CommandHandler{
 	Name:    "suspend",
 	Aliases: []string{"unsuspend"},
 	Func: func(ce *CommandEvent) {
-		err := ce.Meta.Bot.SynapseAdmin.SuspendAccount(ce.Ctx, id.UserID(ce.Args[0]), synapseadmin.ReqSuspendUser{
-			Suspend: ce.Command != "unsuspend",
-		})
+		var err error
+		if ce.Meta.Bot.SpecVersions.Supports(mautrix.FeatureAccountModeration) {
+			_, err = ce.Meta.Bot.UnstableSetSuspendedStatus(ce.Ctx, id.UserID(ce.Args[0]), ce.Command == "suspend")
+		} else {
+			err = ce.Meta.Bot.SynapseAdmin.SuspendAccount(ce.Ctx, id.UserID(ce.Args[0]), synapseadmin.ReqSuspendUser{
+				Suspend: ce.Command != "unsuspend",
+			})
+		}
 		if err != nil {
 			ce.Reply("Failed to %s: %v", ce.Command, err)
 		} else {
