c07a9aa96ce81581024def199e227cae94078032
timedout <git@nexy7574.co.uk>
2026-07-03 23:01:30 +0100

    policyserver: hold incoming policy server requests until the bot finishes loading (#63)

--- cmd/meowlnir/policyserver.go
+++ cmd/meowlnir/policyserver.go
@@ -52,6 +52,7 @@ func (m *Meowlnir) PostMSC4284LegacyEventCheck(w http.ResponseWriter, r *http.Re
 		mautrix.MNotJSON.WithMessage("Request body is not valid JSON").Write(w)
 		return
 	}
+	<-m.startupComplete
 	var ok bool
 	m.MapLock.RLock()
 	eval, ok := m.EvaluatorByProtectedRoom[parsedPDU.RoomID]
@@ -116,6 +117,7 @@ func (m *Meowlnir) postPolicyServerSign(w http.ResponseWriter, r *http.Request, 
 		mautrix.MNotJSON.WithMessage("Request body is not valid JSON").Write(w)
 		return
 	}
+	<-m.startupComplete
 	var ok bool
 	m.MapLock.RLock()
 	eval, ok := m.EvaluatorByProtectedRoom[parsedPDU.RoomID]
