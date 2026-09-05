10734594836d723e60b8598107748b755db3d5c9
timedout <git@nexy7574.co.uk>
2026-09-05 22:16:44 +0100

    policyeval/commands: Add "redact user" shortcut in deactivate command (#56)
    Co-authored-by: Tulir Asokan <tulir@maunium.net>

--- policyeval/commands.go
+++ policyeval/commands.go
@@ -1235,6 +1235,7 @@ var cmdSuspend = &CommandHandler{
 
 type DeactivateParams struct {
 	UserID id.UserID `json:"user"`
+	Redact bool      `json:"redact"`
 	Erase  bool      `json:"erase"`
 }
 
@@ -1245,11 +1246,47 @@ var cmdDeactivate = &CommandHandler{
 		Key:    "user",
 		Schema: cmdschema.PrimitiveTypeUserID.Schema(),
 	}, {
+		Key:      "redact",
+		Schema:   cmdschema.PrimitiveTypeBoolean.Schema(),
+		Optional: true,
+	}, {
 		Key:      "erase",
 		Schema:   cmdschema.PrimitiveTypeBoolean.Schema(),
 		Optional: true,
 	}},
 	Func: commands.WithParsedArgs(func(ce *CommandEvent, args *DeactivateParams) {
+		if args.Redact {
+			task, err := ce.Meta.Bot.SynapseAdmin.RedactUser(ce.Ctx, args.UserID, synapseadmin.ReqRedactUser{})
+			if err != nil {
+				ce.Reply("Failed to start user redaction task: %v", err)
+				return
+			}
+			r := ce.React(ActionPendingReaction)
+			for {
+				progress, err := ce.Meta.Bot.SynapseAdmin.RedactUserStatus(ce.Ctx, task.RedactID)
+				if err != nil {
+					ce.Reply("User redaction task failed: %v", err)
+					return
+				}
+				switch progress.Status {
+				case "complete":
+					if len(progress.FailedRedactions) > 0 {
+						ce.Reply("User redaction task finished successfully but failed to redact %d events", len(progress.FailedRedactions))
+					}
+				case "failed", "cancelled":
+					ce.Reply("User redaction task failed.")
+				case "active":
+					select {
+					case <-time.After(5 * time.Second):
+					case <-ce.Ctx.Done():
+						return
+					}
+					continue
+				}
+				break
+			}
+			_, _ = ce.Meta.Bot.RedactEvent(ce.Ctx, ce.RoomID, r)
+		}
 		err := ce.Meta.Bot.SynapseAdmin.DeactivateAccount(ce.Ctx, args.UserID, synapseadmin.ReqDeleteUser{
 			Erase: args.Erase,
 		})
